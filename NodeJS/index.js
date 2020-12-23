0478f475cdacd83c5e9cbc44bcd4049d52cb785f
Hareeshkg123 <hareeshkg04@gmail.com>
2020-12-23 20:08:42 +0530

    backend finished

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors =require('cors');
 
 const { mongoose } = require('./app');
-var userRouter = require('./router/userRouter');
+var userRouter = require('./controllers/userControllers');
 
 var app = express();
 app.use(bodyParser.json());
+app.use(cors({origin:'http://localhost:4200'}));
 
 app.listen(3000, () => console.log('Server started'));
 
